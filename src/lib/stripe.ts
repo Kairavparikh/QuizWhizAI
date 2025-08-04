161c05f9914c65762205f699929126046983f7c3
Kairavparikh <kairav.parikh01@gmail.com>
2025-08-04 16:25:48 -0400

    kl

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,10 @@
 import Stripe from "stripe";
 
-export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY || " ",
-  {
-    apiVersion: "2023-10-16",
-    typescript: true,
-  }
-);
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing Stripe secret key in environment variables.");
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: "2023-10-16",
+  typescript: true,
+});
